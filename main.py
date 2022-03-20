c8b8a2533ac27e04308d50877bd65fb09f52b5e4
Vinicius Oliveira <vinicius.oliveira1399@gmail.com>
2022-03-20 19:10:34 -0300

    teste-deploy

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def pokeapi():
-    return "Hello world!"
+    return "Hello world"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
